4ec0e1549d4b0fbb8f87ffb19bdcc952e3a80377
JJongho123 <dam33534@naver.com>
2025-01-03 15:49:30 +0900

    환경 구축 완룍
    - jdk 17
    - spring boot 3.3.7
    - gradle 7.5
    - mysql 8.x
    - mybatis

---
+++ src/main/java/com/soccer/config/MyBatisConfig.java
@@ -0,0 +1,42 @@
+package com.soccer.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.soccer.user.mappers")
+public class MyBatisConfig {
+
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/soccer"); // DB URL
+//        dataSource.setUsername("root"); // DB 사용자명
+//        dataSource.setPassword("root"); // DB 비밀번호
+//        return dataSource;
+//    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setTypeAliasesPackage("com.soccer.user.models");
+
+        factoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/UserMapper.xml"));
+
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
--- src/main/java/com/soccer/user/mappers/UserMapper.java
+++ src/main/java/com/soccer/user/mappers/UserMapper.java
@@ -1,11 +1,9 @@
 package com.soccer.user.mappers;
 
 import com.soccer.user.models.User;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
 public interface UserMapper {
     List<User> findAllUsers();
     User findById(int id);
