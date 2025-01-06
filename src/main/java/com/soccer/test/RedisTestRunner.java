754dc381a8fefe0d4bdd9eb1d9e9adf96b18de36
JJongho123 <dam33534@naver.com>
2025-01-06 16:52:37 +0900

    redis 연동 테스트 코드 추가
    redis 연동 테스트 코드 추가

---
+++ src/main/java/com/soccer/test/RedisTestRunner.java
@@ -0,0 +1,19 @@
+package com.soccer.test;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisTestRunner implements CommandLineRunner {
+
+    private final RedisTest redisTestService;
+
+    public RedisTestRunner(RedisTest redisTestService) {
+        this.redisTestService = redisTestService;
+    }
+
+    @Override
+    public void run(String... args) {
+        redisTestService.testRedisConnection();
+    }
+}
