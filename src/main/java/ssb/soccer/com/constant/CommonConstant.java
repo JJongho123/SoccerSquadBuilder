9f6d9bd1b15f22c87c3e1832dfefbadd0f797bc6
JJongho123 <dam33534@naver.com>
2025-03-07 17:06:10 +0900

    GPT 관련 기능 추가
    - GPT 응답 화면에 노출 구현
    - GPT 프롬프트 섬세하게 수정
    - GPT 관련 상수 추가

--- src/main/java/ssb/soccer/com/constant/CommonConstant.java
+++ src/main/java/ssb/soccer/com/constant/CommonConstant.java
@@ -1,9 +1,19 @@
 package ssb.soccer.com.constant;
 
 public class CommonConstant {
-    public final static int EXPIRY_DURATION_SECONDS = 3600;  // session, cookie 만료 시간 (초)
-    public final static String USER_KEY = "user";           // redis hashmap 사용자 정보 key
-    public final static  String API_URL = "https://api.openai.com/v1/chat/completions";
-    public final static  String API_KEY = "";
+    // session, cookie 만료 시간 (초)
+    public final static int EXPIRY_DURATION_SECONDS = 3600;
+
+    // Redis Hashmap 사용자 정보 key
+    public final static String USER_KEY = "user";
+
+    // GPT 관련 상수
+    public final static String API_URL = "https://api.openai.com/v1/chat/completions";
+    public final static String API_KEY = "";
+    public final static String GPT_MODEL = "gpt-3.5-turbo";
+    public final static String GPT_ROLE = "user";
+    public final static int MAX_TOKENS = 1000;
+    public final static double TEMPERATURE = 0.3;
+    public final static double TOP_P = 0.3;
 
 }
